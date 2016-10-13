35025192f326da559191e903618b9c6d9ac54bfc
Elise Kain <elise.kain@gmail.com>
2016-10-12 21:30:18 -0400

    add ron-swanson route

---
+++ src/components/ron.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Ron extends Component {
+    
+    constructor() {
+        super()
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>Ron Swanson Typing</h2>
+                <ol>
+                    <li>Rectangle</li>
+                    <li>America</li>
+                    <li>Megaphone</li>
+                </ol>
+                <img src="../../assets/ron-typing.gif"/>
+            </div>
+        )
+    }
+}
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ class App extends React.Component {
 				<br />
 				<ul>
 					<li><Link to="/todo">Todos</Link></li>
+					<li><Link to="/ron-swanson">Ron Typing</Link></li>
 				</ul>
 
 				{this.props.children}
@@ -23,10 +24,12 @@ import NoMatch from '../src/components/nomatch.jsx'
 
 //Todo
 import Todo from '../src/components/todo.jsx'
+import Ron from '../src/components/ron.jsx'
 
 render((<Router history={hashHistory}>
 	<Route name="app" path="/" component={App}>
 		<Route name="todo" path="todo" component={Todo} />
+		<Route name="ron-swanson" path="ron-swanson" component={Ron} />
 	</Route>
 	<Route key="404" path="*" component={NoMatch} />
 </Router>), document.getElementById('root'))
